3043954b6f8ca01f4bbc0b78b35c39380d5718ff
Afro_dite_codes <venuschappie@gmail.com>
2023-12-11 16:38:57 +0200

    my wiring is working correctly with Top 5 Banks

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def welcome():
     
     print(welcome_message)
     
+def country_notFound():
+    message = str("Unfortunately this country does not have any banks listed in the Top 100 Banks in the World\nPlease try again.")
+    return message
+    
 def take_instructions():
     
     while True:
-        command = input("Please enter a command: ")
+        command = input("Please enter a country and argument Eg. : ")
         handle_instruction(command)
 
 def help():
@@ -35,38 +39,44 @@ def help():
     table.add_row(["Top 5 Banks Germany", "Show the top 5 banks in Germany"])
     
     return table.get_string()
+
+def call_all(country, count):
+    print("I got to call all function")
+    toppi = dt.top5_banks(country, count)
+    # dt.lessthan_500US_Bancorp(country, count)
+    # dt.morethan_500US_Bancorp(country, count)
+    # dt.how_many_banks_in_top100(country, count)
+    # dt.top10_banks(country, count=10)
  
 
 def handle_instruction(comm):
-    command = comm.title()
+    
+    # print("I made it to handle instruction")
+    
+    country_list = dt.data_countries()
+    
+    blem = comm.split(" ")
+
+    # Assigning the country and count from the user input
+    country = blem[0]
+    if len(blem) > 1:
+        count = blem[1]
+    
+    # Standardizing the input from the user 
+    command = country.title()
+    print(command)
+    
+    # Handling the command from the user.
     if command == "Help":
         help_box = help()
         return print(help_box)
-    elif command == "Top 5 Countries":
-        # Done
-        return dt.most_freq_countries()
-    elif command == "Top 5 Banks":
-        # Done
-        return dt.top5_banks()
     elif command == "Full Data":
-        # Done
         return dt.view_full_data()
-    elif command == "Below 500 Billion Assets":
-        # Done
-        return dt.lessthan_500US_Bancorp()
-    elif command == "Above 500 Billion Assets":
-        # Done
-        return dt.morethan_500US_Bancorp()
-    elif command == "Top 10 Banks China":
-        # Done
-        return dt.top10_banks_china()
-    elif command == "Top 10 Banks USA":
-        # Done
-        return dt.top10_banks_US()
-    elif command == "Top 10 Banks Japan":
-        return dt.top10_banks_japan()
-    elif command == "Top 5 Banks Germany":
-        return dt.top5_banks_germany()
+    elif command not in country_list:
+        return print(country_notFound())
+    elif command in country_list:
+        print("I passed the condition that checks if the country exists")
+        return call_all(command, count) 
     else:
         print("\nSorry we do not understand that command, please try again.\n")
         
@@ -74,8 +84,8 @@ def handle_instruction(comm):
 def rungame():
     welcome()
     take_instructions() 
-    
-
+    # dt.lessthan_500US_Bancorp("US")
+    # dt.data_countries()
 
     
 def ascii_art(fn):
--- wrangled_data.py
+++ wrangled_data.py
@@ -1,16 +1,18 @@
 import pandas as pd
-import matplotlib as plt
-import numpy as np
 import matplotlib.pyplot as pl
 import openpyxl as op
-from scipy.stats import gaussian_kde
-from scipy import stats
-
-
 
 
 file_path = "top100banks.xlsx"
 raw_data = pd.read_excel(file_path)
+
+def data_countries():
+    list_of_countries = []
+    data_country = raw_data["country"]
+    for i in data_country:
+        list_of_countries.append(i)
+    # print(list_of_countries)
+    return list_of_countries
 
 
 def view_full_data():
@@ -24,87 +26,91 @@ def view_full_data():
     pl.show()
     
     
-def scatter_top5_banks():
-    
+def scatter_top5_banks(country, count=5):
+    # count = int(count)
     # View the top 5 banks in the world represented in a scatter plot
-    top5_banks = raw_data.sort_values(by="total_assets_us_b", ascending=False).head(5)
-    tryy = top5_banks.plot(x="bank", y="total_assets_us_b", kind="bar", rot=6, fontsize=5)
-    tryy.set_xlabel("Bank")
-    tryy.set_ylabel("Total Assets (US Bullion)")
+    top5_banks = raw_data[raw_data["country"] == country].sort_values(by="total_assets_us_b", ascending=False).head(int(count))
+    ax = top5_banks.plot(x="bank", y="total_assets_us_b", kind="bar", rot=6, fontsize=5)
+    ax.set_xlabel("Bank")
+    ax.set_ylabel("Total Assets (US Bullion)")
     pl.show()
     
 
-def top5_banks():
+def top5_banks(country, count=5):
+    
+    print("made it into top 5 banks function")
     
     # See the top 5 banks in the world represented in the terminal
-    top_5_assets = raw_data.sort_values(by="total_assets_us_b", ascending=False).head(5)
+    top_5_assets = raw_data[raw_data["country"] == country].sort_values(by="total_assets_us_b", ascending=False).head(int(count))
     banks_assets = top_5_assets[["bank", "total_assets_us_b"]]
     banks_assets["rank"] = banks_assets.index + 1
     banks_assets.set_index("rank", inplace=True)
     print(banks_assets)
-    scatter_top5_banks()
+    
+    # Pandas visualization of the data
+    scatter_top5_banks(country, count="5")
     
 
-def most_freq_countries():
+def how_many_banks_in_top100(country, count=5):
     
     # Terminal Based data representation
-    five_countries = raw_data[["country", "bank", "total_assets_us_b"]]
-    five_countries["rank"] = raw_data["rank"]
-    fiver = five_countries.set_index("rank")
+    banks_ = raw_data[raw_data["country"] == country][["country", "bank", "total_assets_us_b"]]
+    banks_["rank"] = raw_data["rank"]
+    fiver = banks_.set_index("rank")
     print(fiver.head(5))
     
     # Pandas visualization of the data
-    five_countries.set_index("country", inplace=True)
-    ax = five_countries.plot.area(stacked=True, figsize=(10,6))
-    pl.title("Area Plot of Top Five Countries")
+    banks_.set_index("country", inplace=True)
+    ax = banks_.plot.area(stacked=True, figsize=(10,6))
+    pl.title("Area Plot of Country Banks in The Top 100")
     pl.xlabel("Countries")
     pl.ylabel("Total Assets (US Bullion")
     pl.legend(loc="best")
     pl.show()
 
 
-def lessthan_500US_Bancorp():
+def lessthan_500US_Bancorp(country,count=5):
     
     # Terminal Based data representation
-    less_than = raw_data[raw_data["total_assets_us_b"] < 500].head(20)
+    less_than = raw_data[(raw_data["total_assets_us_b"] < 500) & (raw_data["country"] == country)].head(int(count))
     less_t = less_than.reset_index(drop=True) 
     print(less_t)
     
     # Pandas visualization of the data
-    filtered_data = raw_data[raw_data["total_assets_us_b"] <= 500]
-    less_500 = filtered_data.groupby("bank")["total_assets_us_b"].sum().sort_values(ascending=False).head(20)
+    filtered_data = raw_data[(raw_data["total_assets_us_b"] <= 500) & (raw_data["country"] == country)]
+    less_500 = filtered_data.groupby("bank")["total_assets_us_b"].sum().sort_values(ascending=False).head(int(count))
     ax = less_500.plot(kind="barh", fontsize=6)
     ax.set_xlabel("Total Assets (US Bullion)")
     ax.set_ylabel("Bank")
     pl.show()
     
     
-def morethan_500US_Bancorp():
+def morethan_500US_Bancorp(country, count=5):
     
     # Terminal Based data representation
-    more_than = raw_data[raw_data["total_assets_us_b"] > 500].head(20)
+    more_than = raw_data[(raw_data["total_assets_us_b"] > 500) & (raw_data["country"] == country)].head(int(count))
     more_t = more_than.reset_index(drop=True) 
     print(more_t)
     
     # Pandas visualization of the data
-    filtered_data = raw_data[raw_data["total_assets_us_b"] >= 500]
-    less_500 = filtered_data.groupby("bank")["total_assets_us_b"].sum().sort_values(ascending=False).head(20)
+    filtered_data = raw_data[(raw_data["total_assets_us_b"] >= 500) & (raw_data["country"] == country)]
+    less_500 = filtered_data.groupby("bank")["total_assets_us_b"].sum().sort_values(ascending=False).head(int(count))
     ax = less_500.plot(kind="barh", fontsize=6)
     ax.set_xlabel("Total Assets (US Bullion)")
     ax.set_ylabel("Bank")
     pl.show()
     
     
-def top10_banks_china():
+def top10_banks(country, count=10):
     
     # Terminal Based data representation
-    china_banks = raw_data[raw_data["country"] == "China"].head(10)
-    china = china_banks.reset_index(drop=True)
-    print(china)
+    _banks = raw_data[raw_data["country"] == country].head(int(count))
+    dem_banks = _banks.reset_index(drop=True)
+    print(dem_banks)
     
     # Pandas vizualization of the data
-    banks = china_banks["bank"]
-    assets = china_banks["total_assets_us_b"]
+    banks = _banks["bank"]
+    assets = _banks["total_assets_us_b"]
     pl.figure(figsize=(11,8))
     pl.pie(assets, labels=banks, autopct='%1.1f%%', startangle=140)
     pl.title("Top 10 Banks in China")
